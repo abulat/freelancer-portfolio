5b5eca415559a9a0d2a3d3e1b8bd42c20e94f36a
A. Bulat <alexeybulat@gmail.com>
2025-06-04 11:21:06 +0200

    updating tags

--- src/js/data/clients.js
+++ src/js/data/clients.js
@@ -3,25 +3,25 @@ window.clients = {
         "name": "ABN AMRO",
         "role": "Software Quality and CICD Coach",
         "description": "Was coaching development teams on DevOps/TestOps, test automation, integrating testing into Azure DevOps pipelines, and training engineers on Risk-based testing approach.",
-        "tags": ["banking", "devops", "cicd", "test automation"]
+        "tags": ["banking", "devops", "cicd", "test automation", "GenAI"]
     },
     "client-deel": {
         "name": "Deel",
         "role": "QA Automation Tech Lead",
         "description": "Was leading, mentoring and inspiring a team of 25+ QA Engineers embedded within development teams, driving improvements in effectiveness, and supporting Deel’s successful scaling in a rapidly evolving fintech environment.",
-        "tags": ["fintech", "qa", "leadership"]
+        "tags": ["fintech", "qa", "leadership", "test automation", "mentoring"]
     },
     "client-epo": {
         "name": "European Patent Office (EPO)",
         "role": "QA Lead / Scrum Master",
         "description": "Was leading QA efforts for developing one of the department's key products (Ansera). The role required close collaboration with team members and stakeholders to establish strong development and QA processes.",
-        "tags": ["patent office", "scrum", "qa"]
+        "tags": ["patent office", "scrum", "qa", "test automation"]
     },
     "client-evbox": {
         "name": "EVBox",
         "role": "Principal Software Engineer, Quality Assurance and Automation",
         "description": "Was improving effectiveness of QA processes and approaches for testing of backend and frontend services, increasing test coverage, maintainability, observability and overall stability of automated test solutions, in order to ensure high quality of all provided by EVBox applications and services.",
-        "tags": ["energy", "qa", "automation"]
+        "tags": ["energy", "qa", "test automation"]
     },
     "client-thunderhead": {
         "name": "Thunderhead",
