5e161bb014a675fc6b3a2a63d13f5a3d0cbe497a
abulat <alexeybulat@gmail.com>
2025-06-05 10:58:00 +0000

    Deploying to gh-pages from @ abulat/freelancer-portfolio@ca10af85fdee3a71ffccbefd1168a551a10ee310 🚀

--- js/data/testimonials.js
+++ js/data/testimonials.js
@@ -2,7 +2,7 @@ window.testimonials = [
     {
         name: "Bart K.",
         company: "ABN AMRO Bank N.V.",
-        message: "Aliaksei is a very competent tester/coach/testautomation expert, but more than that he is a vibrant personality, warm, enthusiastic and always cheerful. Next to his knowledge and expertise this made him a very valuable asset to the team!"
+        message: "Aliaksei is a very competent tester / coach / testautomation expert, but more than that he is a vibrant personality, warm, enthusiastic and always cheerful. Next to his knowledge and expertise this made him a very valuable asset to the team!"
     },
     {
         name: "Viktor A.",
